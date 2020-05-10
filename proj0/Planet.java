86fccd7befbc17112c43509cae2c9ced04a2d9ca
Zhang <yuqingzh@38f9d369f2de.ant.amazon.com>
2020-05-09 22:34:38 -0700

    Change G variable again

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,13 +1,14 @@
 import java.lang.Math; 
 
 public class Planet{
+	private static final double G = 6.67e-11;
 	public double xxPos;
 	public double yyPos;
 	public double xxVel;
 	public double yyVel;
 	public double mass;
 	public String imgFileName;
-	private static final double G = 6.67e-11;
+	
 
 	public Planet(double xP, double yP, double xV, double yV, double m, String img){
 		xxPos = xP;
